51d8c938bf2b68438a8a06ef66f7bb746ce4b279
Haruka <hfujii1013@gmail.com>
2023-04-22 02:41:17 -0700

    done section 18

---
+++ Section18/src/store/counter.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialCounterState = { counter: 0, showCounter: true };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export const counterActions = counterSlice.actions;
+
+export default counterSlice.reducer;
+//don't export the entire slice but just it's reducer part by doing that in indexJS
