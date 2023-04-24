a9f2db10c4778f7473f5b015f2e329e8e66ee049
Haruka <hfujii1013@gmail.com>
2023-04-23 20:10:28 -0700

    done Section 19

--- Section19-2/src/store/cart-slice.js
+++ Section19-2/src/store/cart-slice.js
@@ -1,11 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
     replaceCart(state, action) {
@@ -16,6 +16,7 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -37,56 +38,27 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        "https://food-order-redux-fcc5f-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-    };
-
-    try {
-      await sendRequest();
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
+
+//state.changed = true;
+//The changed property is used to track whether the cart has been modified or not.
+//It is initially set to false in the initialState. When an item is added or removed from the cart,
+//the changed property is set to true indicating that the cart has been modified. This property can be used
+//to optimize performance by only sending the cart data to the server if it has been changed.
+
+//changed is a state property in the cartSlice that tracks whether any change has been made to the cart items.
+//It is initially set to false in the initial state.
+//When a user adds, removes or updates items in the cart,
+//this property is set to true to indicate that the cart has been modified.
+//This is useful when persisting the cart data to a backend server. If the changed property is true,
+//it means that the cart has to be updated on the server.
+//The changed property is set to true in the addItemToCart and removeItemFromCart reducers
+//when an item is added or removed from the cart respectively.
