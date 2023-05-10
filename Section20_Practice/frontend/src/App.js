96f14ed88d6aa3aa43c705514459073f985af9fa
Haruka <hfujii1013@gmail.com>
2023-05-09 23:59:13 -0700

    added section 20-298

--- Section20_Practice/frontend/src/App.js
+++ Section20_Practice/frontend/src/App.js
@@ -26,7 +26,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventaction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import Rootlayout from "./pages/Root";
 import EventRootlayout from "./pages/EventsRoots";
@@ -50,11 +50,17 @@ const router = createBrowserRouter([
           },
           {
             path: ":eventId",
-            element: <EventDetailPage />,
+            id: "event-detail",
             loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+              },
+              { path: "edit", element: <EditEventPage /> },
+            ],
           },
-          { path: "new", element: <NewEventPage /> },
-          { path: ":eventId/edit", element: <EditEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventaction },
         ],
       },
     ],
--- Section20_Practice/frontend/src/pages/NewEvent.js
+++ Section20_Practice/frontend/src/pages/NewEvent.js
@@ -1,5 +1,32 @@
+import EventForm from "../components/EventForm";
+import { json, redirect } from "react-router-dom";
+
 function NewEventPage() {
-  return <h1>New Event</h1>;
+  return <EventForm />;
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    data: data.get("data"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/jspn",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
+  return redirect("/events");
+}
